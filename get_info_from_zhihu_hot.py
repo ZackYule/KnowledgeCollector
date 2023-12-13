affee9676f733a4caf36e542e1d58500558991ec
miles <leroyaum@icloud.com>
2023-12-13 11:20:28 +0800

    change to local service

--- get_info_from_zhihu_hot.py
+++ get_info_from_zhihu_hot.py
@@ -1,3 +1,4 @@
+import os
 from utils.downloader import get_data_from_feed
 from utils.extractors import html_to_text, rss_extractor
 from utils.processor import add_date_to_folder
@@ -5,7 +6,7 @@ from utils.storage import objects_to_csv, save_to_csv
 
 
 def get_data_from_zhihu_hot():
-    feed_url = 'https://rsshub.app/zhihu/hot'
+    feed_url = 'http://localhost:1200/zhihu/hot'
 
     question_data = get_data_from_feed(feed_url=feed_url)
     question_data = [rss_extractor(v, html_to_text) for v in question_data]
@@ -14,8 +15,8 @@ def get_data_from_zhihu_hot():
     for question_d in question_data:
         question_url = question_d.get('link', '')
         if question_url != '' and 'www.zhihu.com' in question_url:
-            question_feed_url = question_url.replace('www.zhihu.com',
-                                                     'rsshub.app/zhihu')
+            question_feed_url = question_url.replace('https://www.zhihu.com',
+                                                    'http://localhost:1200/zhihu')
             answer_data = get_data_from_feed(feed_url=question_feed_url)
             answer_data = [rss_extractor(v, html_to_text) for v in answer_data]
             for answer_d in answer_data:
@@ -27,8 +28,10 @@ def get_data_from_zhihu_hot():
 
 
 def main():
-    file_path = 'data/zhihu'
-    file_path = add_date_to_folder(file_path)
+    folder_path = 'data/zhihu'
+    file_name = 'answers.csv'
+    folder_path = add_date_to_folder(folder_path)
+    file_path = os.path.join(folder_path, file_name)
 
     data = get_data_from_zhihu_hot()
     objects_to_csv(data, file_path)
